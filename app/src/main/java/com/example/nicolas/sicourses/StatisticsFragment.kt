d47bf95c9e7f893b64913da0fa160133f2f9ab62
l47y <nicolaskeller92@googlemail.com>
2019-01-14 18:13:43 +0100

    changed bar chart and colors of pie chart

--- app/src/main/java/com/example/nicolas/sicourses/StatisticsFragment.kt
+++ app/src/main/java/com/example/nicolas/sicourses/StatisticsFragment.kt
@@ -14,6 +14,11 @@ import com.github.mikephil.charting.charts.PieChart
 import com.github.mikephil.charting.data.*
 import com.github.mikephil.charting.utils.ColorTemplate
 import kotlinx.android.synthetic.main.fragment_statistics.view.*
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.formatter.IAxisValueFormatter
+
+
 
 
 class StatisticsFragment : Fragment() {
@@ -54,23 +59,60 @@ class StatisticsFragment : Fragment() {
             container_layout.removeAllViews()
 
             val chart = BarChart(context)
+            chart.setDrawGridBackground(true)
             chart.setPadding(16, 16, 16, 16)
             chart.layoutParams = params
 
-            val barDataEntrys = ArrayList<BarEntry>()
-            val courses = mybundle.getParcelableArrayList<CourseDataClass>("courses")
+            var barDataEntrys = ArrayList<BarEntry>()
+            var axisLabels = ArrayList<String>()
+            var courses = mybundle.getParcelableArrayList<CourseDataClass>("courses")
+            courses = ArrayList(courses.sortedWith(compareBy({ it.media})))
+
             for (i in 0..courses.size-1) {
-                barDataEntrys.add(BarEntry(courses.get(i).media.toFloat(), i.toFloat()))
+                val media = courses.get(i).media.toFloat()
+                val nombre = courses.get(i).nombre
+                val empresa = courses.get(i).empresa
+                val lugar = courses.get(i).lugar
+                barDataEntrys.add(BarEntry(i.toFloat(), media))
+                val axisString = "$empresa, $lugar"
+                axisLabels.add(axisString)
             }
+
             val set = BarDataSet(barDataEntrys, "bestCourses")
             var colors = ArrayList<Int>()
             for (c in ColorTemplate.VORDIPLOM_COLORS) {
                 colors.add(c)
             }
+
+            val xAxis = chart.xAxis
+            val axisRight = chart.axisRight
+            val axisLeft = chart.axisLeft
+
+            xAxis.setPosition(XAxis.XAxisPosition.BOTTOM)
+            xAxis.setLabelRotationAngle(-90.toFloat())
+            xAxis.setDrawGridLines(false)
+            xAxis.setLabelCount(axisLabels.size);
+
+
+            axisRight.setDrawZeroLine(false)
+            axisRight.setDrawGridLines(false)
+            axisRight.setDrawLabels(false)
+
+            axisLeft.setDrawZeroLine(false)
+            axisLeft.setDrawGridLines(false)
+
+            xAxis.setValueFormatter { value, axis ->
+                val index = value.toInt()
+                axisLabels.get(index)
+            }
+
+
             set.setColors(colors)
             val finalData = BarData(set)
             chart.setData(finalData)
             chart.animateY(1200)
+            chart.setScaleEnabled(false)
+
             chart.invalidate()
             container_layout.addView(chart)
         }
@@ -107,11 +149,16 @@ class StatisticsFragment : Fragment() {
             }
             val set = PieDataSet(pieDataEntrys, "evalDist")
             set.setSliceSpace(7f)
-            var colors = ArrayList<Int>()
-            for (c in ColorTemplate.VORDIPLOM_COLORS) {
-                colors.add(c)
-            }
-            set.setColors(colors)
+       
+            set.setColors(intArrayOf(
+                R.color.gradient10,
+                R.color.gradient9,
+                R.color.gradient8,
+                R.color.gradient7,
+                R.color.gradient6,
+                R.color.gradient5), context)
+
+
             val finalData = PieData(set)
             chart.setData(finalData)
             chart.animateY(1200)
